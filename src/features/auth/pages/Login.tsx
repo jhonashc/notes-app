22d7eaa402c40799401055940c791117917acad9
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-03-10 11:56:09 -0500

    auth feature updated: formik implemented

--- src/features/auth/pages/Login.tsx
+++ src/features/auth/pages/Login.tsx
@@ -1,78 +1,73 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { Form, Formik } from "formik";
+import {
+  CheckboxField,
+  PasswordTextField,
+  TextField,
+} from "../../../components";
+import { loginSchema } from "../validators";
 
 export const Login: React.FC = (): JSX.Element => {
+  const navigate = useNavigate();
+
   return (
     <div className="w-full max-w-sm place-self-center p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-      <form className="space-y-6">
-        <h5 className="text-xl font-medium text-gray-900 dark:text-white">
-          Sign in to our platform
-        </h5>
-        <div>
-          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Email
-          </label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-            placeholder="name@company.com"
-            required
-          />
-        </div>
-        <div>
-          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Password
-          </label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="••••••••"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-            required
-          />
-        </div>
-        <div className="flex items-start">
-          <div className="flex items-start">
-            <div className="flex items-center h-5">
-              <input
-                id="remember"
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validationSchema={loginSchema}
+        onSubmit={(values, actions) => {
+          navigate("/", {
+            replace: true,
+          });
+        }}
+      >
+        {(props) => (
+          <Form onSubmit={props.handleSubmit} className="space-y-6">
+            <h5 className="text-xl font-medium text-gray-900 dark:text-white">
+              Sign in to our platform
+            </h5>
+            <TextField
+              name="email"
+              type="email"
+              label="Email"
+              placeholder="name@company.com"
+            />
+            <PasswordTextField
+              name="password"
+              label="Password"
+              placeholder="••••••••"
+            />
+            <div className="flex items-start">
+              <CheckboxField
+                name="remember"
                 type="checkbox"
-                value=""
-                className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                required
+                label="Remember me"
               />
+              <Link
+                to="#"
+                className="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500"
+              >
+                Lost Password?
+              </Link>
             </div>
-            <label className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-              Remember me
-            </label>
-          </div>
-          <Link
-            to="#"
-            className="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500"
-          >
-            Lost Password?
-          </Link>
-        </div>
-        <Link to="/" className="block">
-          <button
-            type="submit"
-            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Login to your account
-          </button>
-        </Link>
-        <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-          Not registered?
-          <Link
-            to="/auth/register"
-            className="text-blue-700 hover:underline dark:text-blue-500 ml-1"
-          >
-            Create account
-          </Link>
-        </div>
-      </form>
+            <button
+              type="submit"
+              className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Login to your account
+            </button>
+            <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+              Not registered?
+              <Link
+                to="/auth/register"
+                className="text-blue-700 hover:underline dark:text-blue-500 ml-1"
+              >
+                Create account
+              </Link>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
--- src/features/auth/pages/Register.tsx
+++ src/features/auth/pages/Register.tsx
@@ -1,69 +1,61 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { Form, Formik } from "formik";
+import { PasswordTextField, TextField } from "../../../components";
+import { registerSchema } from "../validators";
 
 export const Register: React.FC = (): JSX.Element => {
+  const navigate = useNavigate();
+
   return (
     <div className="w-full max-w-sm place-self-center p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-      <form className="space-y-6">
-        <h5 className="text-xl font-medium text-gray-900 dark:text-white">
-          Sign up to our platform
-        </h5>
-        <div>
-          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Email
-          </label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-            placeholder="name@company.com"
-            required
-          />
-        </div>
-        <div>
-          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Password
-          </label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="••••••••"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-            required
-          />
-        </div>
-        <div>
-          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Confirm password
-          </label>
-          <input
-            type="password"
-            name="confirmPassword"
-            id="confirmPassword"
-            placeholder="••••••••"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-            required
-          />
-        </div>
-        <Link to="/" className="block">
-          <button
-            type="submit"
-            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Register your account
-          </button>
-        </Link>
-        <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-          Do you have an account?
-          <Link
-            to="/auth/login"
-            className="text-blue-700 hover:underline dark:text-blue-500 ml-1"
-          >
-            Login here
-          </Link>
-        </div>
-      </form>
+      <Formik
+        initialValues={{ email: "", password: "", confirmPassword: "" }}
+        validationSchema={registerSchema}
+        onSubmit={(values, actions) => {
+          navigate("/", {
+            replace: true,
+          });
+        }}
+      >
+        {(props) => (
+          <Form onSubmit={props.handleSubmit} className="space-y-6">
+            <h5 className="text-xl font-medium text-gray-900 dark:text-white">
+              Sign up to our platform
+            </h5>
+            <TextField
+              name="email"
+              type="email"
+              label="Email"
+              placeholder="name@company.com"
+            />
+            <PasswordTextField
+              name="password"
+              label="Password"
+              placeholder="••••••••"
+            />
+            <PasswordTextField
+              name="confirmPassword"
+              label="Confirm password"
+              placeholder="••••••••"
+            />
+            <button
+              type="submit"
+              className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Register your account
+            </button>
+            <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+              Do you have an account?
+              <Link
+                to="/auth/login"
+                className="text-blue-700 hover:underline dark:text-blue-500 ml-1"
+              >
+                Login here
+              </Link>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
