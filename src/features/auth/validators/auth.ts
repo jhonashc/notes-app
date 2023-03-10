e6d343737541183a3652220e850979dec27a5e25
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-03-10 11:55:33 -0500

    auth featured updated: form validator added

---
+++ src/features/auth/validators/auth.ts
@@ -0,0 +1,22 @@
+import * as Yup from "yup";
+
+export const loginSchema = Yup.object({
+  email: Yup.string()
+    .email("Please enter a valid email.")
+    .required("This field is required."),
+  password: Yup.string()
+    .min(8, "Password must be at least 8 characters long.")
+    .required("This field is required."),
+});
+
+export const registerSchema = Yup.object({
+  email: Yup.string()
+    .email("Please enter a valid email.")
+    .required("This field is required."),
+  password: Yup.string()
+    .min(8, "Password must be at least 8 characters long.")
+    .required("This field is required."),
+  confirmPassword: Yup.string()
+    .required("This field is required.")
+    .oneOf([Yup.ref("password")], "Passwords do not match."),
+});
